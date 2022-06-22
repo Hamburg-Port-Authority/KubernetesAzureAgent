5ad1b9f70035d3c65b74b1a8a403bf2c573fbf1c
deB4SH <git@b4sh.de>
2022-06-22 13:32:01 +0200

    add: basic tag script

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
-LAST_TAG=$(git describe --abbrev=0 --tags HEAD)
-echo "Building azure agent image with tag $LAST_TAG"
+IS_DEV_BUILD=$(git tag -l --contains HEAD)
+GIT_TAG=$(git describe --abbrev=0 --tags HEAD)
 
-docker \
-    build . \
-    -f src/docker/Dockerfile \
-    -t ghcr.io/deb4sh/kubernetes-azure-agent:$LAST_TAG
+if [ -z "$IS_DEV_BUILD"]
+then
+    TIMESTAMP=$(date +%s)
+    TAG=$(echo "$GIT_TAG"-"$TIMESTAMP")
+else 
+    TAG=GIT_TAG
+fi
+
+echo "Building azure agent image with tag $TAG"
+
+#docker \
+#    build . \
+#    -f src/docker/Dockerfile \
+#    -t ghcr.io/deb4sh/kubernetes-azure-agent:$LAST_TAG
     
