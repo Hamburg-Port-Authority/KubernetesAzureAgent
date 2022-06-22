d360bbc48dbdae210115308202507740f3f722cf
deB4SH <git@b4sh.de>
2022-06-22 13:34:21 +0200

    fix: var

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 IS_DEV_BUILD=$(git tag -l --contains HEAD)
 GIT_TAG=$(git describe --abbrev=0 --tags HEAD)
 
-if [ -z "$IS_DEV_BUILD"]
+if [ -z "$IS_DEV_BUILD" ]
 then
     TIMESTAMP=$(date +%s)
     TAG=$(echo "$GIT_TAG"-"$TIMESTAMP")
 else 
-    TAG=GIT_TAG
+    TAG=$GIT_TAG
 fi
 
 echo "Building azure agent image with tag $TAG"
