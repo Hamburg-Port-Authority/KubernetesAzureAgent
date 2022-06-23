e325e551188bec8fa9ffc228e5469159ae96c3f6
deB4SH <git@b4sh.de>
2022-06-23 09:07:23 +0200

    add: copy init scripts to rw directory

--- src/docker/scripts/entrypoint.sh
+++ src/docker/scripts/entrypoint.sh
@@ -9,11 +9,15 @@ do
 done
 
 # print debug
+echo "[x]====== 0.1 - copy setup scripts to rw-dir"
+mkdir /tmp/container-init
+cp -R -p $filepath /tmp/container-init
+# print debug
 echo "[x]====== 1.0 - calling setup scripts for further setup"
 #add executable tag to all scripts in setup dir
-find $filepath -name "*.sh" -exec chmod +x {} \;
+find /tmp/container-init -name "*.sh" -exec chmod +x {} \;
 #execute all scripts in setup dir
-find $filepath -name "*.sh" -exec sh -c {} \;
+find /tmp/container-init -name "*.sh" -exec sh -c {} \;
 # print debug
 echo "[x]====== 2.0 - start azure client"
 sh -c /azp/start.sh
